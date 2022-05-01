a9652b6c1bf07786a9afcd4272dce45d69059b43
marcelo oliveira <marcelolynx@gmail.com>
2022-05-01 19:08:07 -0400

    iniciando configuracao do EFCore

---
+++ src/services/NWA.Identidade.API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NWA.Identidade.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
