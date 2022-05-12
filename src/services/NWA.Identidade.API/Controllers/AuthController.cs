f5f3891dd099f4ce4949860ce9fd266597965afe
marcelo oliveira <marcelolynx@gmail.com>
2022-05-11 20:18:57 -0400

    Criando UserViewModel para o Identity

---
+++ src/services/NWA.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NWA.Identidade.API.Models;
+using System.Threading.Tasks;
+
+namespace NWA.Identidade.API.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AuthController(SignInManager<IdentityUser> signManager, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signManager;
+        }
+       
+
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if(result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest(result);
+        }
+
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha,
+                false, true);
+
+            if(result.IsCompletedSuccessfully)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+             
+        }
+    }
+}
---
+++ src/services/NWA.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NWA.Identidade.API.Models
+{
+    public class UsuarioRegistro
+    {
+        [Required(ErrorMessage = "O campo {0} obrigatorio.")]
+        [EmailAddress(ErrorMessage = "O Campo {0} esta em formato invalido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas nao conferem.")]
+        public string SenhaConfirmacao { get; set; }
+    }
+
+    public class UsuarioLogin
+    {
+        [Required(ErrorMessage ="O campo {0} e obrigatorio")]
+        [EmailAddress(ErrorMessage = "O Campo {0} esta em formato invalido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="O campo e obrigatorio")]
+        [StringLength (100, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength =6)]
+        public string Senha { get; set; }
+    }
+}
